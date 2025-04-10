8dcaef1b5fa969d5604261c872f71cbcfd0b7094
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2025-04-10 18:13:33 +0200

    feat: add button text

--- src/partials/Pricing.tsx
+++ src/partials/Pricing.tsx
@@ -17,9 +17,9 @@ export function Pricing() {
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
               href="#contact"
-              className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-primary shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
             >
-              Get started
+              Me contacter 🙂
             </a>
           </div>
           <svg
